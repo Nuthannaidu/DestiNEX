37e5e1eb8adbbc8b12d23cfaae1026d8cd5c34a6
Nuthannaidu <siddubade65@gmail.com>
2025-06-13 22:22:29 +0530

    Update: Fix backend CORS settings and Google OAuth redirect

--- app.js
+++ app.js
@@ -66,8 +66,8 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    sameSite: "lax",
-    secure: false, // set to true if using HTTPS in production
+    sameSite:"none",
+    secure: true, // set to true if using HTTPS in production
     maxAge: 1000 * 60 * 60 * 24,
   }
 }));
