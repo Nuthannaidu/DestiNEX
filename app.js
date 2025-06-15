18039549d103def966ea633fa21f594bfa94bd67
Nuthannaidu <siddubade65@gmail.com>
2025-06-15 20:15:56 +0530

    debugging

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+app.set('trust proxy', 1);
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
